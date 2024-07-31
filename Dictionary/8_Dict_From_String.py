af92e95c1a15794ab3d18fd27fa4bf5c1cbbffd1
Deven5656 <devengupta0987@gmail.com>
2024-07-31 11:07:17 +0530

    [Deven] Add :Python program to create a dictionary from a string.

---
+++ Dictionary/8_Dict_From_String.py
@@ -0,0 +1,36 @@
+"""
+    @Author: Deven Gupta
+    @Date: 30-07-2024 
+    @Last Modified by: Deven Gupta
+    @Last Modified time: 30-07-2024 
+    @Title: Python program to create a dictionary from a string.
+    
+"""
+
+
+def string_to_dict(word):
+    """
+    Description: 
+        This function creates a dictionary from a string.
+    Parameters:
+        word: string to be converted
+    Return:
+        dictionary: dictionary from a string
+    """
+    string_dict = {}
+    for char in word:
+        if char in string_dict:
+            string_dict[char] += 1
+        else:
+            string_dict[char] = 1
+
+    return string_dict
+
+
+def main():
+    word = input("Enter the word to be converted to dictionary: ")
+    print(f"\nThe dictionary for the word {word} is: \n{string_to_dict(word)}")
+
+
+if __name__ == "__main__":
+    main()
