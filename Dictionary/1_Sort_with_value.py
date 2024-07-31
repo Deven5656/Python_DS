199cedf890a78ef43c0634ffcea614917dcb6809
Deven5656 <devengupta0987@gmail.com>
2024-07-31 11:05:53 +0530

    [Deven] Add :Python Program to sort a dictionary by value (ascending and descending)

---
+++ Dictionary/1_Sort_with_value.py
@@ -0,0 +1,40 @@
+"""
+    @Author: Deven Gupta
+    @Date: 30-07-2024 
+    @Last Modified by: Deven Gupta
+    @Last Modified time: 30-07-2024 
+    @Title: Python Program to sort a dictionary by value (ascending and descending)
+    
+"""
+
+
+def sort_dict_by_value(dictionary, reverse=False):
+    """
+    Description: 
+        This function sorts a dictionary by its values.
+    Parameters:
+        dictionary: The dictionary to be sorted.
+        reverse: Boolean to determine sorting order (False for ascending, True for descending).
+    Return:
+        dict: A new dictionary sorted by values.
+    """
+    return dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=reverse))
+
+
+def main():
+    my_dict = {'java': 5, 'rust': 2, 'c': 8, 'python': 1, 'c++': 3}
+    
+    print("Original dictionary:")
+    print(my_dict)
+
+    ascending_dict = sort_dict_by_value(my_dict)
+    print("\nDictionary sorted in ascending order by value:")
+    print(ascending_dict)
+
+    descending_dict = sort_dict_by_value(my_dict, reverse=True)
+    print("\nDictionary sorted in descending order by value:")
+    print(descending_dict)
+
+
+if __name__ == "__main__":
+    main()
