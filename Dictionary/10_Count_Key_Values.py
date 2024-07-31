63d29f38031c76b0c0f9586da6a15fcc683f20cf
Deven5656 <devengupta0987@gmail.com>
2024-07-31 11:07:47 +0530

    [Deven] Add :Python program to count the values associated with key in a dictionary.

---
+++ Dictionary/10_Count_Key_Values.py
@@ -0,0 +1,44 @@
+"""
+    @Author: Deven Gupta
+    @Date: 30-07-2024 
+    @Last Modified by: Deven Gupta
+    @Last Modified time: 30-07-2024 
+    @Title: Python program to count the values associated with key in a dictionary.
+    
+"""
+
+
+def count_dict_key_values(my_dict, target_key):
+    """
+    Description: 
+        This function count the values associated with key in a dictionary.
+    Parameters:
+        my_dict: dictionary
+        target_key = key to be search
+    Return:
+        int: count of value
+    """
+    count = 0
+    for key, value in my_dict.items():
+        for ikey, ivalue in value.items():
+            if ikey == target_key and ivalue == True:
+                count += 1
+
+    return count
+
+
+def main():
+    my_dict = {
+        1: {'id': 1, 'success': True, 'name': 'Prayag'}, 
+        2: {'id': 2, 'success': False, 'name': 'Shiv'}, 
+        3: {'id': 3, 'success': True, 'name': 'Om'},
+        4: {'id': 4, 'success': True, 'name': 'Ayush'},
+        5: {'id': 5, 'success': True, 'name': 'Deven'}
+    }
+
+    key = input("Enter the key whose value needs to be counted: ")
+    print(f"\nThe unique values in the dictionary are: {count_dict_key_values(my_dict, key)}")
+
+
+if __name__ == "__main__":
+    main()
